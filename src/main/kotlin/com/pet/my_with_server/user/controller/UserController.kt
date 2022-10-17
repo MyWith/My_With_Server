8ffefc2a810ec86e2be1d9edd25c50fa4185e3d7
nGyu <vpdls1511@gmail.com>
2022-10-17 23:47:54 +0900

    feat(vpdls1511) : DTO 생성

--- src/main/kotlin/com/pet/my_with_server/user/controller/UserController.kt
+++ src/main/kotlin/com/pet/my_with_server/user/controller/UserController.kt
@@ -1,6 +1,8 @@
 package com.pet.my_with_server.user.controller
 
+import com.pet.my_with_server.user.dto.UserDto
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController
 class UserController {
 
     @PostMapping("/login")
-    fun loginUser(): String {
-        return "login"
+    fun loginUser(@RequestBody user:UserDto): UserDto {
+        val a:String = "TEST"
+        return user
     }
 }
---
+++ src/main/kotlin/com/pet/my_with_server/user/dto/UserDto.kt
@@ -0,0 +1,6 @@
+package com.pet.my_with_server.user.dto
+
+data class UserDto(
+    val username: String,
+    val password: String,
+)
